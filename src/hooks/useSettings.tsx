0d880992b17ebef9b268d9467343c6fab198655d
ndhlam05 <nguyenhuuduylam05@gmail.com>
2024-03-25 22:48:48 +0700

    remove no need components

--- src/hooks/useSettings.tsx
+++
@@ -1,4 +0,0 @@
-import { useContext } from 'react'
-import { SettingsContext, SettingsContextValue } from 'src/contexts/SettingsContext'
-
-export const useSettings = (): SettingsContextValue => useContext(SettingsContext)
